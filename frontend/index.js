db6c5ebc9c11d26dccbb36cc439d0e15483b48b1
scarter31191 <scarter31191@gmaile.com>
2021-04-04 16:50:49 -0400

    create item form

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,18 @@
 const reportList = document.getElementById('report-list') // best practice is to run this line in the console and call it to see if its pulling the target
 const itemsAdapter = new ItemsAdapter
+const itemForm = document.getElementById('item-form')
+const itemName = document.getElementById('item-name')
+const itemDescription = document.getElementById('item-description')
+const itemNumber = document.getElementById('item-number')
+const orderQty = document.getElementById('order-qty')
+const damageQty = document.getElementById('damage-qty')
 
 
-// function fetchItems(){
-//     fetch('http://localhost:3000/items')
-//     .then(res => res.json())
-//     .then(data => addItems(data))
-// }
+function fetchItems(){
+    fetch('http://localhost:3000/items')
+    .then(res => res.json())
+    .then(data => addItems(data))
+}
 
 function addItems(resp){
     resp.data.forEach(item => 
@@ -18,16 +24,34 @@ function addItemToDOM(item){
     reportList.innerHTML += `
     <div id="report-${item.id}">
         <li>
-            <span class="name">Name: ${item.attributes.name}</span><br>
-            <strong class="description">Description: ${item.attributes.description}</strong><br>
-            <span class="item_number">Item Number: ${item.attributes.item_number}</span><br>
-            <strong class="description">Order QTY: ${item.attributes.order_qty}</strong><br>
-            <strong class="description">Damage QTY: ${item.attributes.damage_qty}</strong><br><br>
+            <span class="name">Name: ${item.attributes.name}</span>
+            <strong class="description">Description: ${item.attributes.description}</strong>
+            <span class="item_number">Item Number: ${item.attributes.item_number}</span>
+            <strong class="description">Order QTY: ${item.attributes.order_qty}</strong>
+            <strong class="description">Damage QTY: ${item.attributes.damage_qty}</strong>
         </li>
+        <button class="delete" data-id="${this.id}">Delete</button>
+        <button class="update" data-id="${this.id}">Update</button>
     </div>
     `
 }
 
+function handleFormSubmit(e){
+    e.preventDefault() // with forms the page will refresh by default this will prevent that
+    debugger
+    
+    let newItemObj = {
+        name: itemName.value,
+        description: itemDescription.value,
+        number: itemNumber.value,
+        order_qty: orderQty.value,
+        damage_qty: damageQty.value
+    }
+    console.log(newItemObj)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    itemsAdapter.fetchItems()
+    // itemsAdapter.fetchItems()
+    fetchItems()
+    itemForm.addEventListener('submit', handleFormSubmit)
 })
