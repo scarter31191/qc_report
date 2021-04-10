b72d4b6422195974305cafa6cc81caa4f1945776
scarter31191 <scarter31191@gmaile.com>
2021-04-10 12:25:28 -0400

    added employee_id to serializer

--- backend-report/app/serializers/item_serializer.rb
+++ backend-report/app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :item_number, :order_qty, :damage_qty, :id
+  attributes :name, :description, :item_number, :order_qty, :damage_qty, :id, :employee_id
   belongs_to :employee
 end
